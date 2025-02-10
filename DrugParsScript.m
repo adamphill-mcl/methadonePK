cb484ea2e13b5d8f9262ca68bd82f35dd32ff58a
adamphill-mcl <adamandclare@me.com>
2025-02-10 17:40:57 +1100

    edited to include Cliff's measured values

--- DrugParsScript.m
+++ DrugParsScript.m
@@ -1,4 +1,4 @@
-%make table of IC50s etc
+%make table of IC50s etc. These are taken from Cliff's syncropatch data
 
 parameters =    ["IKr_methR"; 
                 "IKr_methS";
@@ -10,25 +10,25 @@ parameters =    ["IKr_methR";
                 "ICaL_methS";
                 "ICaL_metab"];
 
-        IC50s = [7e-6;
-                7e-6;
-                7e-6;
-                31.8e-6;
-                31.8e-6;
-                31.8e-6;
-                37.4e-6;
-                37.4e-6;
-                37.4e-6];
+        IC50s = [1.89e-6;
+                2.09e-6;
+                3.29e-5;
+                4.5e-6;
+                9.34e-6;
+                4.13e-5;
+                2.08e-5;
+                1.34e-5;
+                1.19e-4];
 
-            h = [0.82;
-                0.82;
-                0.82
-                1.37;
-                1.37;
-                1.37;
-                1.67;
-                1.67;
-                1.67];
+            h = [0.7631;
+                0.8279;
+                0.7941;
+                0.988;
+                1.411;
+                1.562;
+                0.754;
+                0.6664;
+                0.9776];
 DrugPars = table(IC50s,h,RowNames=parameters);
 save('DrugPars.mat', 'DrugPars');
     
