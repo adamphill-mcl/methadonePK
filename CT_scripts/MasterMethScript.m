32d41cfa46d44c44419615b7a96b9475faa463d6
adamphill-mcl <adamandclare@me.com>
2026-03-31 12:15:55 +1100

    Remove obsolete CT_scripts methadone model assets

--- CT_scripts/MasterMethScript.m
+++
@@ -1,9 +0,0 @@
-for formulation = ["R"]
-    for CypScore = [0, 0.5, 1, 1.5, 2]
-        for RF = [0.25, 0.5, 0.75, 1]
-            for BW = [50, 60, 70, 80, 90]
-                ModelMain_Loewe_CT2(formulation, CypScore, RF, BW)
-            end
-        end
-    end
-end
